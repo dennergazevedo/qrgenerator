44727f82c387baa3c719fb26ba1296fc9a3793bb
Denner Azevedo <dennergazevedo@gmail.com>
2020-11-19 12:42:20 -0300

    FINAL GENERATOR

---
+++ src/App.jsx
@@ -0,0 +1,81 @@
+/* eslint-disable no-restricted-globals */
+import React, { useState } from 'react';
+
+// STYLED COMPONENTS
+
+import {
+  Container,
+  BackLeft,
+  BackRight,
+  Navbar,
+  Body,
+  QrCode
+} from './styles.js';
+
+// ASSETS
+import backleft from './assets/img/backleft.png';
+import backright from './assets/img/backright.png';
+import logo from './assets/img/logo.png';
+import background from './assets/img/background.png';
+
+// ICONS
+import {
+  FaQrcode
+} from 'react-icons/fa';
+
+function App() {
+
+  const [texto, setTexto] = useState('');
+  const [exibir, setExibir] = useState(false);
+  const [hidden, setHidden] = useState(true);
+
+  function handleGerar(){
+    setHidden(false);
+    setExibir(true);
+  }
+
+  return (
+    <Container>
+      {
+        screen.width > 600 || window.innerWidth > 600?
+        <>
+          <BackLeft src={backleft} alt="BACKGROUND"/>
+          <BackRight src={backright} alt="BACKGROUND"/>
+        </>
+        :
+        null
+      }
+      <Navbar>
+        <img src={logo} alt="LOGO" />
+        <span>GERADOR DE QRCODE</span>
+      </Navbar>
+      <Body 
+        animate={{ y: 15, opacity: 1 }}
+        transition={{ ease: 'easeOut', duration: 1 }} >
+        <img 
+          className="background"
+          src={background} 
+          alt="background"/>
+
+        <input
+          placeholder="Insira aqui o seu site ou texto." 
+          value={texto}
+          onChange={e => setTexto(e.target.value)}/>
+
+        <button onClick={handleGerar}>
+          <FaQrcode className="icon"/>
+          GERAR QRCODE
+        </button>
+
+        <QrCode 
+          style={{visibility: `${hidden? 'hidden': 'visible'}`}}
+          width="100px" 
+          src={`https://vps-3922624.artcopias.com.br:1498/qr_code/${exibir}`} 
+          alt="QRCODE" 
+          height="100px"/>
+      </Body>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/styles.js
@@ -0,0 +1,116 @@
+import styled from 'styled-components';
+import { motion } from "framer-motion";
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    max-width: 100vw;
+    font-family: 'Montserrat';
+`;
+
+export const BackRight = styled.img`
+    width: 40vw;
+    height: auto;
+    display: flex;
+    position: absolute;
+    right: 0;
+    bottom: 0;
+`;
+
+export const BackLeft = styled.img`
+    width: 40vw;
+    height: auto;
+    display: flex;
+    position: absolute;
+    left: 0;
+    bottom: 0;
+`;
+
+export const Navbar = styled(motion.div)`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    width: 90vw;
+    height: 60px;
+    padding-left: 30px;
+    padding-right: 30px;
+
+    img{
+        width: auto;
+        height: 40px;
+    }
+
+    span{
+        font-size: 16px;
+        font-weight: 600;
+    }
+`;
+
+export const Body = styled(motion.div)`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin-top: 10px;
+
+    .divQr{
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .background{
+        width: 30vw;
+        min-width: 250px;
+        height: auto;
+    }
+
+    input{
+        width: 30vw;
+        min-width: 300px;
+        height: auto;
+        padding: 5px 5px 5px 20px;
+        height: 50px;
+        border: 2px solid #3337;
+        font-size: 20px;
+        margin-top: 50px;
+        border-radius: 5px;
+        font-weight: 600;
+    }
+
+    button{
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        font-size: 12px;
+        font-weight: 600;
+        color: #FFF;
+        background-color: #712BC3;
+        border: none;
+        padding: 15px;
+        padding-left: 30px;
+        padding-right: 30px;
+        margin-top: 20px;
+        cursor: pointer;
+
+        &:hover{
+            background-color: #491785;
+        }
+
+        .icon{
+            font-size: 16px;
+            margin-right: 10px;
+        }
+    }
+`;
+
+export const QrCode = styled(motion.img)`
+    display: flex;
+    width: 200px;
+    height: 200px;
+    margin-top: 20px;
+`;
